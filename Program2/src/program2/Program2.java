72eab03172dbba25b68e2aeba67d6e0c5d102de1
Linda <34050926+linsogles@users.noreply.github.com>
2018-01-25 15:23:35 -0600

    Adding first batch

---
+++ Program2/src/program2/Program2.java
@@ -0,0 +1,23 @@
+/*
+ * Linda Oglesbee & Scott Mitchell
+ * 2/6/2018
+ * Program 2
+ * This class interacts with user to display
+ * employee's of  information of the Beam Me Up! Transportation Company 
+ */
+package program2;
+
+/**
+ *
+ * @author Linda
+ */
+public class Program2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
